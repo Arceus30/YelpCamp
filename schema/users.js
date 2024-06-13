97005a00c312c00ed254374f02e17741951e3953
KESHAV <keshavkhandelwal30@gmail.com>
2024-06-13 14:36:23 +0530

    Schema update

--- schema/users.js
+++ schema/users.js
@@ -1,38 +1,13 @@
-const BaseJoi = require("joi");
-const sanitizeHtml = require("sanitize-html");
-
-const extension = (joi) => ({
-    type: "string",
-    base: joi.string(),
-    messages: {
-        "string.escapeHTML": "{{#label}} must not include HTML!",
-    },
-    rules: {
-        escapeHTML: {
-            validate(value, helpers) {
-                const clean = sanitizeHtml(value, {
-                    allowedTags: [],
-                    allowedAttributes: {},
-                });
-                if (clean !== value)
-                    return helpers.error("string.escapeHTML", { value });
-                return clean;
-            },
-        },
-    },
-});
-
-// Create a Joi instance with the custom extension
-const Joi = BaseJoi.extend(extension);
+const Joi = require("joi");
 
 const registerSchema = Joi.object({
-    username: Joi.string().min(1).required().escapeHTML(),
-    email: Joi.string().min(1).required().escapeHTML(),
-    password: Joi.string().min(1).required().escapeHTML(),
+    username: Joi.string().min(1).required(),
+    email: Joi.string().min(1).required(),
+    password: Joi.string().min(1).required(),
 });
 const loginSchema = Joi.object({
-    username: Joi.string().min(1).required().escapeHTML(),
-    password: Joi.string().min(1).required().escapeHTML(),
+    username: Joi.string().min(1).required(),
+    password: Joi.string().min(1).required(),
 });
 
 module.exports = { registerSchema, loginSchema };
