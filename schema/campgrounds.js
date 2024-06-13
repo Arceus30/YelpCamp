8963a8cea4dbe8685525ecae01e787f48e797866
KESHAV <keshavkhandelwal30@gmail.com>
2024-06-13 14:40:33 +0530

    Schema update

--- schema/campgrounds.js
+++ schema/campgrounds.js
@@ -1,36 +1,11 @@
-const BaseJoi = require("joi");
-const sanitizeHtml = require("sanitize-html");
-
-const extension = (joi) => ({
-    type: "string",
-    base: joi.string(),
-    messages: {
-        "string.escapeHTML": "{{#label}} must not include HTML!",
-    },
-    rules: {
-        escapeHTML: {
-            validate(value, helpers) {
-                const clean = sanitizeHtml(value, {
-                    allowedTags: [],
-                    allowedAttributes: {},
-                });
-                if (clean !== value)
-                    return helpers.error("string.escapeHTML", { value });
-                return clean;
-            },
-        },
-    },
-});
-
-// Create a Joi instance with the custom extension
-const Joi = BaseJoi.extend(extension);
+const Joi = require("joi");
 
 const campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().min(1).required().escapeHTML(),
+        title: Joi.string().min(1).required(),
         price: Joi.number().min(0).required(),
-        location: Joi.string().min(1).required().escapeHTML(),
-        description: Joi.string().min(1).required().escapeHTML(),
+        location: Joi.string().min(1).required(),
+        description: Joi.string().min(1).required(),
         // image: Joi.string().min(1).required(),
     }).required(),
     deleteImages: Joi.array(),
--- schema/reviews.js
+++ schema/reviews.js
@@ -1,34 +1,9 @@
-const BaseJoi = require("joi");
-const sanitizeHtml = require("sanitize-html");
-
-const extension = (joi) => ({
-    type: "string",
-    base: joi.string(),
-    messages: {
-        "string.escapeHTML": "{{#label}} must not include HTML!",
-    },
-    rules: {
-        escapeHTML: {
-            validate(value, helpers) {
-                const clean = sanitizeHtml(value, {
-                    allowedTags: [],
-                    allowedAttributes: {},
-                });
-                if (clean !== value)
-                    return helpers.error("string.escapeHTML", { value });
-                return clean;
-            },
-        },
-    },
-});
-
-// Create a Joi instance with the custom extension
-const Joi = BaseJoi.extend(extension);
+const Joi = require("joi");
 
 const reviewSchema = Joi.object({
     review: Joi.object({
-        body: Joi.string().min(1).required().escapeHTML(),
-        rating: Joi.number().min(0).required().max(5),
+        body: Joi.string().min(1).required(),
+        rating: Joi.number().min(0).required(),
     }).required(),
 });
 
