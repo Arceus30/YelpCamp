2f3137ab33a6a1e6753b8a743bb83a63b2c8deda
KESHAV <keshavkhandelwal30@gmail.com>
2024-06-13 14:45:16 +0530

    Schema update

--- app.js
+++ app.js
@@ -90,6 +90,7 @@ app.use((req, res, next) => {
     if (req.originalUrl != "/login") {
         req.session.returnTo = req.originalUrl;
     }
+    console.log(res.locals.currentUser);
     next();
 });
 
